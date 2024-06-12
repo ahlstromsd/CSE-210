5a9cc5b7bd371d6a998cc38e83549dde880c99cf
Sydney Ahlstrom <ahl19001@byui.edu>
2024-06-11 21:26:43 -0600

    part 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        if (grade > 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (grade > 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (grade > 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (grade > 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F.");
+        }
     }
 }
