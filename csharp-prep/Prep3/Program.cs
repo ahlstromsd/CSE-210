8b8514907df32da5ec2360968d0b26a590eccc63
Sydney Ahlstrom <ahl19001@byui.edu>
2024-06-17 10:53:55 -0600

    yayyyy

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        while (guess!= magicNumber)
+        {
+            Console.WriteLine("what is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        // Console.WriteLine("What is the magic number?");
+        // string magic_number = Console.ReadLine();
+        // int number = int.Parse(magic_number);
+        // Console.WriteLine("What is your guess?");
+        // string guess = Console.ReadLine();
+        // int guess1 = int.Parse(guess);
+        // while (guess1 != number)
+        // {
+        // if (guess1 == number)
+        // {
+        //     Console.WriteLine("That's correct!");       
+        // }
+        // else if (guess1 > number)
+        // {
+        //     Console.WriteLine("Too high!");
+        // }
+        // else if (guess1 < number)
+        // {
+        //     Console.WriteLine("Too low!");
+        // }
+        // }
     }
 }
